6ecf40f677c075a63f265236793ebc16b6fec8ee
nader chargui <charguinader19901403@gmail.com>
2021-05-31 20:02:12 +0200

    new  update  for protfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Wave from 'react-wavify';
+import Typewriter from 'react-simple-typewriter'
+import 'react-simple-typewriter/dist/index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div> 
+    <div className='App'>
+    <h1
+    style={{ paddingTop: '5rem', margin: 'auto 0', fontWeight: 'normal' }}
+    >
+    I'm a {' '}
+    <span style={{ color: '#1277b0', fontWeight: 'bold' }}>
+      {/* Style will be inherited from the parent element */}
+      <Typewriter
+        loop
+        cursor
+        cursorStyle='_'
+        typeSpeed={70}
+        deleteSpeed={50}
+        delaySpeed={1000}
+        words={['Developer','front end', 'back end']}
+      />
+    </span>
+    </h1>
+
+    <Wave fill='#1277b0'
+    paused={false}
+    options={{
+      height: 80,
+      amplitude: 40,
+      speed: 0.20,
+      points: 4
+    }}
+/>
+
+
+            
+</div>
+
+</div>
+
+
+
+    
   );
 }
 

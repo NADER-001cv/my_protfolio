02fc72e2773f9b07dbda0753556247af83f1ce0b
nader chargui <charguinader19901403@gmail.com>
2021-06-01 03:41:43 +0200

    repeat

--- src/layout/headernav/Nav.js
+++ src/layout/headernav/Nav.js
@@ -1,26 +1,26 @@
-import React from 'react'
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-function Nav() {
-    return (
-        <div>
-        <ul className="nav justify-content-end">
-        <li className="nav-item">
+// import React from 'react'
+// // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// function Nav() {
+//     return (
+//         <div>
+//         <ul className="nav justify-content-end">
+//         <li className="nav-item">
         
-          <Nav.Link  className="nav-link active" aria-current="page"   href="#home">Home</Nav.Link>
+//           <Nav.Link  className="nav-link active" aria-current="page"   href="#home">Home</Nav.Link>
 
-        </li>
-        <li className="nav-item">
-          <Nav.Link  className="nav-link" href="#">Link</Nav.Link>
-        </li>
-        <li className="nav-item">
-          <Nav.Link  className="nav-link" href="#">Link</Nav.Link>
-        </li>
-        <li className="nav-item">
-          <Nav.Link  className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</Nav.Link>
-        </li>
-      </ul>
-        </div>
-    )
-}
+//         </li>
+//         <li className="nav-item">
+//           <Nav.Link  className="nav-link" href="#">Link</Nav.Link>
+//         </li>
+//         <li className="nav-item">
+//           <Nav.Link  className="nav-link" href="#">Link</Nav.Link>
+//         </li>
+//         <li className="nav-item">
+//           <Nav.Link  className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</Nav.Link>
+//         </li>
+//       </ul>
+//         </div>
+//     )
+// }
 
-export default Nav
+// export default Nav
